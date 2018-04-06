e7186e6aa42cfc2a2833e6e70aebb8585111525e
evsystem <evsystem@localhost.localdomain>
2018-04-06 09:18:39 -0400

    first commit

---
+++ 1.Persiapan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+########################################################################
+# LEARN-GIT SCRIPT
+# CREATED   : EKO SAPUTRA (EVSYSTEM)
+# DEV       : XENTA OS http://www.xentaos.org/
+# LICENSE   : GNU GPL 3.0
+# WEBSITE   : https://evsystemsite.wordpress.com
+########################################################################
+
+###Persiapan
+ \
+#buat repositori baru
+jalankan perintah
+ev@evsystem ~/$ git init
